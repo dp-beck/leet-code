6f183fbfef6101caca2c8561c5243d037e9ef738
daniel-beck <beckdp01@gmail.com>
2024-12-21 15:43:42 -0500

    Add solution for Two Sum

---
+++ Program.cs
@@ -0,0 +1,9 @@
+﻿using leet_code;
+
+// Two Sum
+var twoSumSolution = new TwoSumSolution();
+var result = twoSumSolution.TwoSum(new int[] { 2, 7, 11, 15 }, 9);
+
+Console.WriteLine("Two Sum");
+Console.WriteLine("Input: [2, 7, 11, 15], Target: 9");
+Console.WriteLine($"Result: [{result[0]}, {result[1]}]");
